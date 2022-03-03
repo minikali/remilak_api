e94826228c396c8855530313f5e87e0870023d55
Remi LAK <remilak11@gmail.com>
2022-03-03 22:54:07 +0100

    clean db config

--- config/database.js
+++ config/database.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: "mysql",
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'db_remilak'),
-      user: env('DATABASE_USERNAME', 'remilak'),
-      password: env('DATABASE_PASSWORD', 'remilak@Farfar31'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      ssl: env.bool("DATABASE_SSL"),
     },
   },
 });
